ec06817cc962a6a8f7445fbbe914f9be043a6f2c
nanako-mori <nanako.mori19871229@gmail.com>
2024-01-16 11:50:01 +0900

    1Lあたりの価格 レイアウト修正

--- js/script.js
+++ js/script.js
@@ -24,13 +24,13 @@ $.ajax({
       // "数字 + L", "数字 + l", "数字 + リットル"という文字列があれば、その前の数字を抜き出す
       // \d+(?:\.\d+)?: 符号なし0省略なしの実数
       const capacity = productName.match((/(\d+(?:\.\d+)?L)|(\d+(?:\.\d+)?l)|(\d+(?:\.\d+)?リットル)/g));
-      let pricePerLiterMes = '計算不可。もしくは猫砂ではありません。';
+      let pricePerLiterMes = `計算不可。<br>もしくは猫砂ではありません。`;
       let capacityNum = 0;
       if(capacity != null) {
         capacityNum = capacity[0].slice(0, -1);
         const pricePerLiter = parseInt(productPrice)/parseFloat(capacityNum);
         const roundedPrice = Math.round(pricePerLiter);
-        pricePerLiterMes = `1Lあたりの価格は<span class="emphasis">${roundedPrice}円</span>です。`;
+        pricePerLiterMes = `<span class="big-emphasis">${roundedPrice}円</span>`;
       }
 
       const product = `<li class="product-detail">
@@ -40,14 +40,15 @@ $.ajax({
           <th>商品画像</th>
           <th>価格</th>
           <th>容量</th>
+          <th>1Lあたりの価格</th>
         </tr>
         <tr>
           <td><img class="thumb" src="${productImg}" /></td>
           <td><span class="big-emphasis">${productPrice}円</span></td>
           <td><span class="big-emphasis">${capacityNum}L</span></td>
+          <td>${pricePerLiterMes}</td>
         </tr>
       </table>
-      <p class="price-per-liter">${pricePerLiterMes}</p>
       <p><a target="_blank" href="${shopURL}">ショップ名: ${shopName}</a></p>
       </li>`;
       $('#products').append(product);
