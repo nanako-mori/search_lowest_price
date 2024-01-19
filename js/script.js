e589149238893efe7a52e1a87c7cec4136117f97
nanako-mori <nanako.mori19871229@gmail.com>
2024-01-19 14:26:25 +0900

    最安値の算出と最安値マークの表示

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ $.ajax({
   }
 })
 .done(function(data){
-  let min = Infinity, max = -Infinity;
   data.Items.forEach(function(item, index){
     // 猫砂以外が検索結果に表示されるのを防ぐため、ジャンルIDが猫砂の場合に限定する
     if(item.Item.genreId === "204174") {
@@ -21,7 +20,6 @@ $.ajax({
       const productImg = item.Item.mediumImageUrls[0].imageUrl;
       const shopURL = item.Item.shopUrl;
       const shopName = item.Item.shopName;
-      let lowestPriceMark = '';
 
       // "数字 + L", "数字 + l", "数字 + リットル"という文字列があれば、その前の数字を抜き出す
       // \d+(?:\.\d+)?: 符号なし0省略なしの実数
@@ -32,25 +30,11 @@ $.ajax({
         capacityNum = capacity[0].slice(0, -1);
         const pricePerLiter = parseInt(productPrice)/parseFloat(capacityNum);
         const roundedPrice = Math.round(pricePerLiter);
-        pricePerLiterMes = `<span class="big-emphasis">${roundedPrice}円</span>`;
-
-        // 最安値を比較
-        if(roundedPrice < min) {
-          min = roundedPrice;
-        }
-        if(roundedPrice > max) {
-          max = roundedPrice;
-        }
+        pricePerLiterMes = `<span class="exist-price big-emphasis">${roundedPrice}円</span>`;
       }
-      console.log(`max: ${max}`);
-      console.log(`min: ${min}`);
-      // lowestPriceMark = `<div class="lowest-mark-inner">
-      // <div class="lowest-mark"><p>最安値</p></div>
-      // </div>`;
 
       const product = `<li class="product-detail">
-      <p>商品名: <a target="_blank" href="${productURL}">${productName}</a></p>
-      <div class="lowest-mark-wrap">${lowestPriceMark}</div>
+      <p id="lowest-mark">商品名: <a target="_blank" href="${productURL}">${productName}</a></p>
       <table>
         <tr>
           <th>商品画像</th>
@@ -70,6 +54,32 @@ $.ajax({
       $('#products').append(product);
     }
   });
+
+  const str = $('.exist-price').text();
+  const res = str.split('円');
+
+  // 最安値を比較
+  let min = Infinity;
+  for(let i = 0; i < res.length; i++) {
+    const int = parseInt(res[i]);
+    if(int < min) {
+      min = int;
+    }
+  }
+
+  // 最安値マークを付与
+  const elem = $('.exist-price');
+  for(let i = 0; i < elem.length; i++) {
+    const tarText = elem.eq(i).text();
+    if(tarText.indexOf(min +'円') > -1) {
+      const lowestPriceMark = `<div class="lowest-mark-wrap">
+      <div class="lowest-mark-inner">
+        <div class="lowest-mark"><p>最安値</p></div>
+        </div>
+      </div>`;
+      elem.eq(i).closest('table').prev('#lowest-mark').html(lowestPriceMark);
+    }
+  }
 }).fail(function(){
   console.log('取得失敗');
 });
